e63a4b06a5d8cd992f35dfc060c22b32926befa5
Tobias Lee <tobiasle@usc.edu>
2014-02-07 15:01:04 -0800

    Updated Youtube script to allow enabling/disabling

--- youtube.py
+++ youtube.py
@@ -1,5 +1,5 @@
 __module_name__ = "YouTube title" 
-__module_version__ = "1.01" 
+__module_version__ = "1.13" 
 __module_description__ = "Prints the name of a YouTube Video" 
 
 import xchat
@@ -9,6 +9,8 @@ import gdata.youtube.service
 
 def yt_cb(word, word_eol, userdata):
 	chan = xchat.find_context(channel="#commie-subs")
+	if not YT_enabled:
+		return xchat.EAT_NONE
 	if chan is None:
 		return xchat.EAT_NONE
 	for url in word[1].split(' '):
@@ -23,15 +25,31 @@ def yt_cb(word, word_eol, userdata):
 #					if vid.find('&') != -1
 #						vid = vid[:vid.find('&')]
 					vid = url[url.find('v=')+2:url.find('v=')+13]
+					service = gdata.youtube.service.YouTubeService()
 					try:
 						entry = service.GetYouTubeVideoEntry(video_id=vid)
 					except gdata.service.RequestError:
 						xchat.prnt("Invalid Video ID")
 						return xchat.EAT_NONE
 					title = entry.media.title.text
-					chan.command("say YouTube Video Name: " + title)
+					chan.command("say YouTube video title: " + title)
 					return xchat.EAT_NONE
 	return xchat.EAT_NONE
 
-service = gdata.youtube.service.YouTubeService()
+def en_cb(word, word_eol, userdata):
+	global YT_Enabled
+	if word[0].lower() == "elite_soba":
+		if word[1][0:6] == "enable":
+			if word[1][7:9].lower() == "yt":
+				YT_enabled = True
+				xchat.command("msg Elite_Soba YT Module enabled")
+		elif word[1][0:7] == "disable":
+			if word[1][8:10].lower() == "yt":
+				YT_enabled = False
+				xchat.command("msg Elite_Soba YT Module disabled")
+	return xchat.EAT_NONE
+
+
+YT_enabled = True
 xchat.hook_print("Channel Message", yt_cb)
+xchat.hook_print("Private Message to Dialog", en_cb)
